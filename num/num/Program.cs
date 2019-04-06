e2a3b4ecbc1ddc0bf27e5b8d5dc1ba85b2c60c7e
saeed <s.vaezi78@gmail.com>
2019-04-06 23:35:09 +0430

    sec com

--- num/num/Program.cs
+++ num/num/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace num
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] X = new int[10];
+            System.Console.WriteLine("please enter 10 integer");
+            int big = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                X[i] = int.Parse(System.Console.ReadLine());
+                if (big < X[i])
+                {
+                    big = X[i];
+                }
+            } //end for
+
+            System.Console.WriteLine("big is:" + big);
+            Console.ReadKey();
+
         }
     }
 }
