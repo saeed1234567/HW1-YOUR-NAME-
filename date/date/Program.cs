ebfd97562060fafddb434c2252d2e649001c076e
saeed <s.vaezi78@gmail.com>
2019-04-06 23:38:03 +0430

    sec com date

--- date/date/Program.cs
+++ date/date/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace date
+namespace tarikh
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Globalization.PersianCalendar pc = new System.Globalization.PersianCalendar();
+
+            Console.WriteLine((pc.GetYear(DateTime.Now) + "/" + pc.GetMonth(DateTime.Now) + "/" + pc.GetDayOfMonth(DateTime.Now)));
+            Console.ReadKey();
         }
     }
 }
